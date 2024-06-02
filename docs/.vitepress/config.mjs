90d1566712e207658edd5a386aa83da9a613543f
wds-dxh <wdsnpshy@163.com>
2024-06-02 13:35:10 +0800

    添加了仓库路径

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "邬东升的博客",
   description: "邬东升，东玄海，嵌入式，机器学习，科技作品，课程设计",
+  base: '/wds-blog/',
   markdown: {
     container: {
       tipLabel: '提示',
