fcfabfa25f06be94d3bb4ab4b1acba666d890e04
wds-dxh <wdsnpshy@163.com>
2024-06-03 00:27:31 +0800

    脚本提交

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -6,17 +6,4 @@ import MyLayout from "./layout/Index.vue";
 export default {
     extends: DefaultTheme,    //继承默认主题
     Layout: MyLayout,
-
-    enhanceApp({ app, router, siteData, isServer }) {  
-      if (!isServer) {  
-        // 客户端注入 meta 标签  
-        app.mount('#app').$nextTick(() => {  
-          const meta = document.createElement('meta');  
-          meta.name = 'msvalidate.01';  
-          meta.content = '5DD22862BBCB585FF26B1F2FB033DEA0'; // 你的验证码  
-          document.head.appendChild(meta);  
-        });  
-      }  
-    }  
-    
   }
