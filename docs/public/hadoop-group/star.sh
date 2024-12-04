b7665a08e04bdafdfccad59b8c6d0067b969a67c
wds-Ubuntu22-cqu <wdsnpshy@163.com>
2024-12-05 01:29:59 +0800

    修改启动脚本

--- docs/public/hadoop-group/star.sh
+++ docs/public/hadoop-group/star.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+# 启动 Docker Compose
+docker-compose up -d
+
 docker stop node1 node2 node3
 #先停止所有容器
-
 
 
 # 启动node1, node2, node3容器
@@ -24,6 +27,14 @@ echo "启动node3的SSH服务..."
 docker exec node3 sudo /usr/sbin/sshd -D &
 sleep 1  # 等待1秒
 echo "node3的SSH服务已启动。"
+
+
+# hadoop namenode -format   
+# 格式化HDFS文件系统，只需要执行一次，如果已经执行过，不需要再次执行
+echo "格式化HDFS文件系统..."
+docker exec node1 /opt/hadoop/bin/hadoop namenode -format
+echo "HDFS文件系统格式化完成。"
+
 
 #启动hadoop程序，在node1容器中
 echo "启动Hadoop程序..."
